961e19830e524f13609eae7f8f01596d17b3aacd
mzbinden-tkt <zbinden.d.m@gmail.com>
2020-12-30 15:02:09 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import words from 'lodash.words';
+import Functions from './components/Functions';
+import MathOperations from './components/MathOperations';
+import Numbers from './components/Numbers';
+import Result from './components/Result';
+import './App.css';
+
+const App = () => {
+  const [stack, setStack] = useState('');
+  const items = words(stack, /[^-^+^*^/]+/g);
+  const value = items.length > 0 ? items[items.length -1 ] : 0;
+  return (
+  <main className='react-calculator'>
+    <Result value={value}/>
+    <Numbers 
+      onClickNumber={ number => setStack(`${stack}${number}`)}
+    />
+    <Functions
+      onContentClear={() => setStack('')} 
+      onDelete={ () => {
+        if(stack.length > 0)
+        {
+          setStack(stack.substring(0, stack.length -1 ))
+        }
+      }
+    }/>
+    <MathOperations 
+      onClickOperation={ operation => setStack(`${stack}${operation}`) } 
+      onClickEqual={ equal => setStack(eval(stack).toString()) }
+    />
+  </main>);
+}
+
+export default App;
